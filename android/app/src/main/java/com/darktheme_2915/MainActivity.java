70d6e449efee17bb7faa3d862bd4872e3aa6caae
Crowdbotics <team@crowdbotics.com>
2019-05-01 05:44:48 +0000

    initial commit

---
+++ android/app/src/main/java/com/darktheme_2915/MainActivity.java
@@ -0,0 +1,15 @@
+package com.darktheme_2915;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "darktheme_2915";
+    }
+}
